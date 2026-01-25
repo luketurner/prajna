f580b06c58d3741ce23ea86726246e4574cf1968
Luke Turner <github@luketurner.org>
2026-01-25 13:52:04 -0800

    calculate expected value for current day of year

--- components/home.tsx
+++ components/home.tsx
@@ -1,4 +1,5 @@
 import { useAppState } from "@/hooks/state";
+import { getDayOfYear } from "date-fns";
 import { useCallback, useEffect } from "react";
 import { Button, Text, View } from "react-native";
 import { useGoals, useSelectedGoal, useUpdateGoal } from "../hooks/goals";
@@ -21,10 +22,28 @@ export default function Home() {
       await updateGoal(selectedGoal.id, { current: selectedGoal.current + 1 });
     }
   }, [selectedGoal, updateGoal]);
+  const current = selectedGoal?.current ?? 0;
+  const target = selectedGoal?.target ?? 0;
+
+  const now = new Date();
+  const dayNumber = getDayOfYear(now);
+  const expectedPerDay = target / 365;
+  const expectedCurrent = expectedPerDay * dayNumber;
+
+  const realPercent = (100 * current) / target;
+  const expectedPercent = (100 * expectedCurrent) / target;
   return (
     <View>
-      <Text>{selectedGoal?.current ?? 0}</Text>
-      <Text>{selectedGoal?.target ?? 0}</Text>
+      <Text
+        style={{
+          marginVertical: 12,
+          marginHorizontal: "auto",
+        }}
+      >
+        status {current}/{target} ({realPercent.toFixed(2)}%) &mdash; day{" "}
+        {dayNumber} &mdash; expected {expectedCurrent.toFixed(2)} (
+        {expectedPercent.toFixed(2)}%)
+      </Text>
       <Button title="Record" onPress={handleRecord}></Button>
     </View>
   );
